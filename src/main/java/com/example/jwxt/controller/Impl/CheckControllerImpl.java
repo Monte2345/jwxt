cef9732c6d8f9fa192853a05ae95a97116893830
lzj666888 <2766006821@qq.com>
2021-01-06 15:01:43 +0800

    添加cors

--- src/main/java/com/example/jwxt/controller/Impl/CheckControllerImpl.java
+++ src/main/java/com/example/jwxt/controller/Impl/CheckControllerImpl.java
@@ -20,39 +20,7 @@ import java.io.IOException;
 public class CheckControllerImpl implements CheckController {
     private static final Logger LOGGER = LoggerFactory.getLogger(CheckControllerImpl.class);
 
-    /*
-     * 当页面请求时，使用画布获取一个验证码图像
-     * 返回到页面
 
-     * 接收前端获取验证码的请求，使用画布随机生成一个四位数的验证码图像
-     * 将生成的四位数字，数字或字母放在一个session域中使得登录验证的controller中可以获取到该值，判断登陆验证
-     * 使用mageIO.write(image, "JPEG", response.getOutputStream());输出图片
-     * 使其再前端可以接收再图片显示出来
-     * */
-    @RequestMapping(value = "/checkCode")
-    public void checkCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //设置编码格式
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-
-        // 设置不缓存图片
-        response.setHeader("Pragma", "No-cache");
-        response.setHeader("Cache-Control", "No-cache");
-        response.setDateHeader("Expires", 0);
-
-        // 指定生成的响应图片,一定不能缺少这句话,否则错误.
-        response.setContentType("image/jpeg");
-        /*
-         * @param
-         * 传入生成图像长度和宽度，还有本次会话的请求HttpServletRequest对象request
-         * @return
-         * 返回一个BufferedImage对象
-         * */
-        BufferedImage image = ImageUtils.getPicture(80, 35, request);
-        // 将图像输出到response输出流中。
-        //response.getOutputStream()获取response输出流
-        ImageIO.write(image, "JPEG", response.getOutputStream()); // 输出图片
-    }
 
     @RequestMapping("/verifyCode")
     public void verifyCode(HttpServletRequest request, HttpServletResponse response) {
